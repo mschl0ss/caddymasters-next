0be936593f0938429f9a9222c656afeeba9090c1
Marc Schlossberg <mr.schlossberg@gmail.com>
2024-05-02 22:39:21 -0400

    Make prisma.ts typesafe

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 
+// eslint-disable-next-line import/no-mutable-exports
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
+  prisma = new PrismaClient();
 } else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
+  const globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalWithPrisma.prisma;
 }
 
 export default prisma;
